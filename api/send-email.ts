c3513ca57b268257294ae8dc5dec48460a71cbdc
leonelnm <elerickmontenegro@gmail.com>
2025-05-27 18:07:39 +0200

    change to deafult

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,4 +1,4 @@
-export async function GET(request: Request) {
+export default async function GET(request: Request) {
   const response = await fetch('https://api.vercel.app/products');
   const products = await response.json();
   return Response.json(products);
